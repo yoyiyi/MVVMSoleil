b4f7e88a55e2438aed4411311e4ef50b68aad76b
zzq <2455676683@qq.com>
2019-08-16 15:41:00 +0800

    init commit

--- mvvm_soleil/src/main/java/com/yoyiyi/soleil/binding/adapter/imageview/BindingAdapter.java
+++ mvvm_soleil/src/main/java/com/yoyiyi/soleil/binding/adapter/imageview/BindingAdapter.java
@@ -1,14 +1,12 @@
-package com.yoyiyi.soleil.binding.viewadapter.view;
+package com.yoyiyi.soleil.binding.adapter.imageview;
 
 /**
  * 类名：ViewAdapter
  * 描述：TODO
- * 时间：2019/8/16 15:13
+ * 时间：2019/8/16 15:33
  *
  * @author <a href="2455676683@qq.com">zzq</a>
  * @version 1.0
  */
-public class ViewAdapter {
-
-
+public class BindingAdapter {
 }
