9a86a85e69c524d355e42bfd6a5a84c0f22ec826
zzq <2455676683@qq.com>
2019-08-16 15:13:22 +0800

    init commit

---
+++ mvvm_soleil/src/main/java/com/yoyiyi/soleil/binding/viewadapter/view/ViewAdapter.java
@@ -0,0 +1,12 @@
+package com.yoyiyi.soleil.binding.viewadapter.view;
+
+/**
+ * 类名：ViewAdapter
+ * 描述：TODO
+ * 时间：2019/8/16 15:13
+ *
+ * @author <a href="2455676683@qq.com">zzq</a>
+ * @version 1.0
+ */
+public class ViewAdapter {
+}
---
+++ mvvm_soleil/src/main/java/com/yoyiyi/soleil/utils/SPUtils.java
@@ -0,0 +1,12 @@
+package com.yoyiyi.soleil.utils;
+
+/**
+ * 类名：SPUtils
+ * 描述：sp工具类
+ * 时间：2019/8/15 13:13
+ *
+ * @author <a href="2455676683@qq.com">zzq</a>
+ * @version 1.0
+ */
+public class SPUtils {
+}
